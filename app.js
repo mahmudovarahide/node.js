e2b205b60733db8377f609b4c0a7d6e541a68502
mahmudova.rahide <mahmudovarahide@gmail.com>
2023-06-24 14:32:16 +0400

    first express app

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require("http");
+const express = require('express');
 
-const routes = require('./routes.jsx');
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    console.log('Product');
+    res.send('<h1>Product</h1>')
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('Birinci Midlware');
+    res.send('<h1>Express JS</h1>')
+});
+
+app.listen(3000);
