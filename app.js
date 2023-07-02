21a76d967140ccba27210e8d8108d3fe7d244818
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-02 13:55:38 +0400

    create html page with express.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const path = require("path");
+
+const express = require("express");
+
+const adminRoutes = require("./routes/admin.js");
+
+const shopRoutes = require("./routes/shop.js");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log('Product');
-    res.send('<h1>Product</h1>')
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-    console.log('Birinci Midlware');
-    res.send('<h1>Express JS</h1>')
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
