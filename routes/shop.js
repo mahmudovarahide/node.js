88a6e4ff685f7dee21d88a86d749ba7a0ec4853e
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-04 14:15:18 +0400

    Products Loop

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,10 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop"});
+
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  res.render('shop');
 });
 
 module.exports = router;
